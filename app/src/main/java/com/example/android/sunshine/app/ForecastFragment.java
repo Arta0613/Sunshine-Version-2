41b786d6c5b406118a66f1c0b7b44dc62c1ae1f7
Mohammed Amin <Mohammed.Amin@viacom.com>
2016-02-24 16:13:11 -0500

    See preferred location on Maps using Implicit Intent

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -250,8 +250,8 @@ public class ForecastFragment extends Fragment {
     }
 
     /* The date/time conversion code is going to be moved outside the asynctask later,
-             * so for convenience we're breaking it out into its own method now.
-             */
+     * so for convenience we're breaking it out into its own method now.
+     */
     private String getReadableDateString(long time) {
         // Because the API returns a unix timestamp (measured in seconds),
         // it must be converted to milliseconds in order to be converted to valid date.
